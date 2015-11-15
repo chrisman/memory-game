00fd7b52f5c15396d8e00b0165a827c702a9f519
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-15 08:08:52 -0700

    data structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
+var data = {
+  corey: ['crazy', 'corey'],
+  chris: ['cool', 'chris'],
+  aubrey: ['awesome', 'aubrey']
+};
+
 $(document).ready(function(){
-  $('#container').html('<h1>HELLLLLLLO</h1>');
+  console.log('doc ready');
+  log_data(data);
 });
+
+var log_data = function(obj){
+  for (var name in obj) {
+    console.log(name + "'s name is " + obj[name][0] + " " + obj[name][1]);
+  }
+}
--- scrpits/main.js
+++
@@ -1,7 +0,0 @@
-var pairs = {
-
-};
-
-$(document).ready(function(){
-  $('#container').html('<h1>HELLLLLLLO</h1>');
-});
