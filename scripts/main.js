f420d18eb80be598b833aa7057f5795feef322ff
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-15 08:24:12 -0700

    string manip

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,20 @@ var data = {
 $(document).ready(function(){
   console.log('doc ready');
   log_data(data);
+  make_divs(data);
 });
+
+// make a string of divs to inserst.
+// make each div have an id equal to an array item from the object
+var make_divs = function(obj){
+  console.log('make some divs');
+  var div_string = [];
+  for (var name in obj) {
+    div_string.push(obj[name][0]);
+    div_string.push(obj[name][1]);
+  }
+  console.log(div_string.join(' '));
+}
 
 var log_data = function(obj){
   for (var name in obj) {
