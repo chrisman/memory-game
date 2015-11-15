92b84a7361c74c00b17dfe2a3c50fd636cae3a61
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-15 09:35:13 -0700

    divs

---
+++ README.md
@@ -0,0 +1,14 @@
+# memory game
+
+You've done this before. You know what's up.
+
+## TODO
+
+1. _selected_ class: animation, color.
+2. _div on click_ event:
+  * can toggle _selected_
+  * if # _selected_ == 2
+    * check data object for contains id pair?
+    * YES: remove elements
+    * NO: unselect all
+3. timer? score?
--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,40 @@ var data = {
 $(document).ready(function(){
   console.log('doc ready');
   log_data(data);
-  make_divs(data);
+  var my_divs = make_divs_with_ids(shuffle(get_ids(data)));
+  console.log(my_divs);
+  $('section').html(my_divs);
 });
 
-// make a string of divs to inserst.
-// make each div have an id equal to an array item from the object
-var make_divs = function(obj){
-  console.log('make some divs');
+// return an array of div elements containing ids
+var make_divs_with_ids = function(a){
+  for (var i = 0; i < a.length; i++){
+    a[i] = "<div id='" + a[i] + "'>&nbsp;</div>";
+  }
+  return a;
+}
+
+// http://bost.ocks.org/mike/shuffle/
+var shuffle = function(array){
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+
+var get_ids = function(obj){
+  console.log('make an array of ids');
   var div_string = [];
   for (var name in obj) {
     div_string.push(obj[name][0]);
     div_string.push(obj[name][1]);
   }
   console.log(div_string.join(' '));
+  return div_string;
 }
 
 var log_data = function(obj){
